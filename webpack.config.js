cb26a6342ca50ee347836c6648226c1af0ad21ae
zhuhui <huizhu@wisedu.com>
2017-11-25 16:49:00 +0800

    添加dev配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
-module.exports = {
+var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+const config = {
   entry: __dirname + '/src/blurry.js',
   output: {
     path: __dirname + "/dist",
@@ -14,3 +16,47 @@ module.exports = {
     }]    
   }
 }
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log(process.env.NODE_ENV)
+  Object.assign(config, {
+    entry: __dirname + '/example/main.js',
+    output: {
+      path: __dirname + "/example/",
+      filename: 'build.js'
+    },
+    devServer:{
+      contentBase: "./", 
+      historyApiFallback:true,
+      inline:true,
+      hot:true
+    },
+    watch: true,
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new HtmlWebpackPlugin({
+        filename: __dirname + '/example/richText.html',
+        template: __dirname + '/example/richText.html',
+        inject: true
+      }),
+      new webpack.DefinePlugin({
+        'process.env': 'development'
+      })
+    ]
+  })
+} else {
+  Object.assign(config, {
+    entry: __dirname + '/src/blurry.js',
+    output: {
+      path: __dirname + "/dist",
+      filename: 'index.js'
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': 'production'
+      })
+    ]
+  })
+}
+
+module.exports = config
